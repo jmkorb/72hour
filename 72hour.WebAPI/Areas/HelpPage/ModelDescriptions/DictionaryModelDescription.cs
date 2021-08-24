a01ecb76ddefde4039b87f6650f2f7a854ef777a
Jacob Korb <jacob.korb@gmail.com>
2021-08-24 19:44:51 -0400

    Add project files.

---
+++ 72hour.WebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace _72hour.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ 72hour.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_72hour.WebAPI.WebApiApplication" Language="C#" %>
