2590447e8b7bf3e2ed8cfc61da7db688fbb3ae85
Nicholas Stoner <nicholas.c.stoner@gmail.com>
2021-08-24 21:01:49 -0400

    added annotations to like class and guid property

--- 72hour.Data/Like.cs
+++ 72hour.Data/Like.cs
@@ -1,6 +1,7 @@
 ﻿using _72hour.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,14 @@ namespace _72hour.Data
 {
     public class Like
     {
+        [Key]
         public int LikeId { get; set; }
 
         [ForeignKey(nameof(Post))]
         public int PostId { get; set; }
         public virtual Post Posts { get; set; }
 
+        [Required]
+        public Guid OwnerId { get; set; }
     }
 }
