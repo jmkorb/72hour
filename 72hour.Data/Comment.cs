8bfe63dfb4c3f97f6a22ff54c8aa442e4584d6a4
Marcus <marcus317@gmail.com>
2021-08-26 16:42:17 -0400

    not migration but fixed my comment.class

--- 72hour.Data/Comment.cs
+++ 72hour.Data/Comment.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using _72hour.Data;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,14 @@ namespace _72hour.Models
         public string Text { get; set; }
 
         public Guid AuthorId { get; set; }
+
+        public virtual List<Reply> Replies { get; set; }
+
+        [ForeignKey(nameof(Post))]
+
+        public int PostId { get; set; }
+
+        public virtual Post Posts { get; set; }
 
 
     }
