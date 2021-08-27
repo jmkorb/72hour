c8dd03673b7e85b359034c2833f0d2c30803c417
Denzel <dbledsoe374@gmail.com>
2021-08-26 20:06:41 -0400

    Working On Services.

---
+++ 72hour.Services/ReplySrevice/ReplyService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _72hour.Services.ReplySrevice
+{
+    class ReplyService
+    {
+        private readonly Guid _authorId;
+        public ReplyService(Guid authorId)
+        {
+            _authorId = authorId;
+        }
+
+    }
+}
