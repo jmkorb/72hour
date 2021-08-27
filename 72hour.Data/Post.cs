055f5136336981447b8f0ce6b27b3b00aac9ac14
Jacob Korb <jacob.korb@gmail.com>
2021-08-26 21:10:42 -0400

    changed Post namespace

--- 72hour.Data/Post.cs
+++ 72hour.Data/Post.cs
@@ -12,13 +12,10 @@ namespace _72hour.Data
     {
         [Key]
         public int Id { get; set; }
-        [Required]
         public string Title { get; set; }
-        [Required]
         public string Text { get; set; }
         public virtual List<Comment> Comments { get; set; }
         public virtual List<Like> Likes { get; set; }
-        [Required]
         public Guid AuthorId { get; set; }
     }
 }
